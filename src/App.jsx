c3ddb84ba9084ca601a1ffef1ed95ca2fbb550cb
Diego J. Varela <Djvarela89@gmail.com>
2023-10-11 20:41:35 -0300

    Correccion de como se guardan los datos en el localhost

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,39 @@ import { ListItem } from "./assets/components/ListItem";
 import { isActive } from "./utils";
 
 function App() {
-
   const [searchCity, setSearchCity] = useState("");
   const [guests, setGuests] = useState("");
-  const {register,handleSubmit} = useForm()
+  const { register, handleSubmit } = useForm();
 
+  const onSearchSubmit = handleSubmit((e) => {
+    setSearchCity(e.city);
+    setGuests(e.number);
+
+    const local = JSON.parse(localStorage.getItem("city"))  || []; //tomar
  
-
-  const onSearchSubmit = handleSubmit((e)=>{
-    setSearchCity(e.city)
-    setGuests(e.number)
-
-    let citys = []
-    const local = JSON.parse(localStorage.getItem('city'))
-    citys.push(e.city);
-
-    localStorage.setItem('city',JSON.stringify(citys))
-  
-    
-    console.log({local})
-    
-
-  })
-
+    local.push(e.city);
+   
+    localStorage.setItem("city", JSON.stringify(local)); // enviar
+    console.log(local)
+  });
 
   return (
     <>
       <header>
         <nav>
           <ul>
-            <form onSubmit={onSearchSubmit} className="forms-modal" autoComplete="off">
-              <input type="text" {...register('city')} 
+            <form
+              onSubmit={onSearchSubmit}
+              className="forms-modal"
+              autoComplete="off"
+            >
+              <input type="text" {...register("city")} onClick={isActive} />
+              <input
+                type="number"
+                {...register("number")}
                 onClick={isActive}
-
-              
+                placeholder="Add guests"
               />
-              <input type="number" {...register('number')}
-                onClick={isActive}
-              placeholder='Add guests'/>
               <button type="submit">🔍</button>
             </form>
           </ul>
@@ -49,9 +44,9 @@ function App() {
         <div className="modal-search">
           <div className="location">
             <ul>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
             </ul>
           </div>
 
@@ -60,16 +55,15 @@ function App() {
               <li>Test</li>
             </ul>
           </div>
-
         </div>
       </header>
 
       <main>
         <h2>Stays in finlandia</h2>
         <h3>12 days</h3>
-          <ul>
-            <ListItem city={searchCity}  guests={guests} />
-          </ul>
+        <ul>
+          <ListItem city={searchCity} guests={guests} />
+        </ul>
       </main>
     </>
   );
